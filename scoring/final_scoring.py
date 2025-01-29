eb4abcab36e75a9dfe43eb52cc465d83b7f30521
Yunkyung Ko <koyunkyung@naver.com>
2025-01-29 14:26:53 +0900

    update output evaluation mechanisms

---
+++ scoring/final_scoring.py
@@ -0,0 +1,76 @@
+import os
+import json
+import openai
+import random
+from llm_scoring import *
+from em_scoring import *
+from mean import *
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from collections import defaultdict
+
+with open("credentials/config.json", "r") as f:
+    config = json.load(f)
+openai.api_key = config.get("OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = config.get("OPENAI_API_KEY")
+
+model = ChatOpenAI(model='gpt-4', temperature=0)
+output_parser = StrOutputParser()
+
+def random_shuffle(data):
+    all_items = []
+    for inference_type, items in data.items():
+        for item in items:
+            item["inference_type"] = inference_type 
+            all_items.append(item)
+
+    random.shuffle(all_items)
+    return all_items
+
+def evaluate_similarity(data):
+    all_items = random_shuffle(data)
+    results = []
+    em_count_dict = defaultdict(int)
+    for item in all_items:
+        output = item["short_output"]
+        answer = item["answer"]
+        inference_type = item["inference_type"]
+
+        em_score = exact_match_score(output,answer)
+        if em_score :
+            score = 5
+            em_count_dict[inference_type] += 1
+        else :
+            score = llm_similarity(output,answer,inference_type)
+        results.append({
+            "output": output,
+            "answer": answer,
+            "score": score,
+            "inference_type": inference_type
+        })
+        print(f"Processed: Output: {output}, Answer: {answer}, Score: {score}, Type: {inference_type}")
+
+    return results, em_count_dict
+
+def save_results_to_file(results, output_file):
+    with open(output_file, 'w', encoding='utf-8') as file:
+        json.dump(results, file, ensure_ascii=False, indent=4)
+
+def main():
+    data_name = "hotpot" 
+    # data_name = "gsm8k"
+    input_file = f"results/{data_name}_results.json"
+    output_file = f"results/{data_name}_scoring_results.json"
+    em_count_output_file = f"results/{data_name}_em_count_results.json"
+
+    with open(input_file, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    
+    results, em_count_dict = evaluate_similarity(data)
+
+    save_results_to_file(results, output_file)
+    save_results_to_file(em_count_dict, em_count_output_file)
+    print(f"Results saved to {output_file}")
+
+if __name__ =="__main__":
+    main()
---
+++ scoring/llm_scoring.py
@@ -0,0 +1,40 @@
+import openai
+import os
+import json
+import random
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+with open("credentials/config.json", "r") as f:
+    config = json.load(f)
+openai.api_key = config.get("OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = config.get("OPENAI_API_KEY")
+
+model = ChatOpenAI(model='gpt-4', temperature=0)
+output_parser = StrOutputParser()
+
+def generate_template(output, answer):
+    template = f"""
+    당신은 아웃풋과 모범답안 두가지를 비교하여, 아웃풋이 모범답안과 얼마나 유사한지 평가해야 합니다.
+
+    ### 아웃풋: "{output}"
+    ### 답안: "{answer}"
+
+    위의 아웃풋과 모범답안이 얼마나 유사한지를 비교하여 0점부터 5점 사이의 점수를 매겨주세요.
+    예를 들어 "I don't have enough information to provide an answer to this query." 이런 아웃풋처럼 요구한 답변을 아무것도 생성하지 못하였다면 0점을 주세요.
+    완벽하게 일치하지 않는다고 하여도 답안의 키워드가 정확하게 포함되어 있다면 높은 점수를 주세요.
+
+    점수만 숫자로 출력하세요.
+    """
+    return template
+
+def llm_similarity(output,answer,inference_type):
+    prompt = generate_template(output, answer)
+        
+    response = model.invoke(prompt) 
+    response_text = response.content.strip() 
+    try:
+        score = float(response_text)
+        return score #output, answer, score, inference_type
+    except ValueError:
+        score = 0.0 
+        return score #output, answer, score, inference_type
